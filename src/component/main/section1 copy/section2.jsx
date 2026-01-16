25d166610fcdca4bc77a59ed63bb0ee9a616c909
komiljonovibrohim223-ally <komiljonovibrohim223@gmail.com>
2026-01-16 15:45:42 +0500

    section2

--- src/component/main/section1 copy/section2.jsx
+++ src/component/main/section1 copy/section2.jsx
@@ -1,4 +1,7 @@
-
+import logo from'../../../../public/logo.png'
+import logo1 from'../../../../public/logo1.png'
+import logo2 from'../../../../public/logo2.png'
+import logo3 from'../../../../public/logo3.png'
 import './section2.css'
 
 function Section2() {
@@ -6,8 +9,28 @@ function Section2() {
 
   return (
     <>
-      
-      
+      <main className='globus'>
+        <section className='features'>
+          <div className='feature'>
+            <h2>water resistant</h2>
+            <img src={logo1} alt="" />
+          </div>
+          <div className='feature'>
+            <h2>charging system</h2>
+            <img src={logo} alt="" />
+          </div>
+          <div className='feature'>
+            <h2>artificial leather</h2>
+            <img src={logo2} alt="" />
+          </div>
+          <div className='feature'>
+            <h2>modern clothes</h2>
+            <img src={logo3} alt="" />
+          </div>
+        </section>
+      </main>
+
+
 
     </>
   )
