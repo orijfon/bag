ca3e44bd49b33811b323edc2ee06a04d96ec5c2d
orijfon <hojeakbar65@gmail.com>
2026-01-16 15:33:48 +0500

    finish

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -7,7 +7,19 @@ function Header() {
   return (
     <>
       
-      
+      <header className='container'>
+        
+          <a className="h14">Bag</a>
+
+          <div className="mini11">
+            <a href="" className='a12'>Home</a>
+            <a href="" className='a12'>Feature</a>
+            <a href="" className='a12'>About</a>
+            <a href="" className='a12'>Product</a>
+            <button className='b64'>Shop Now</button>
+          </div>
+
+      </header>
 
     </>
   )
--- src/component/main/section1/section1.jsx
+++ src/component/main/section1/section1.jsx
@@ -1,3 +1,8 @@
+import strelka from './img/strelka.png'
+import qizcha from './img/qizcha.png'
+import naqw from './img/naqw.png'
+
+
 
 import './section1.css'
 
@@ -7,7 +12,20 @@ function Section1() {
   return (
     <>
       
-      
+      <section className='container s44'>
+
+          <div className="mini98">
+            <div className="p76">Look Stylish Be Stylish.</div>
+            <div className="p75">Look Stylish Be Stylish</div>
+            <div className="p74">Before starting this business you should have ideas about the market and products to Compete with the Existing Businesses.</div>
+            <div className="mini88">
+              <button className='b087'>Join Shop</button>
+              <img className="strelka" src={strelka} alt="" />
+            </div>
+          </div>
+          <img src={qizcha} alt="" className='ph1'/>  
+
+      </section>
 
     </>
   )
