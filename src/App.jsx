61ff6153fe416d8b6055f8ec0a0880ba28b03142
Bilol <bilolninzya19@gmail.com>
2026-01-18 13:22:53 +0500

    footer tugadi

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-
-import './App.css'
-import Footer from './component/footer/footer';
-import Header from './component/header/header';
-import Section3 from './component/main/section1 copy 2/section3';
-import Section4 from './component/main/section1 copy 3/section4';
-import Section5 from './component/main/section1 copy 4/section5';
-import Section2 from './component/main/section1 copy/section2';
-import Section1 from './component/main/section1/section1';
+import "./App.css";
+import Footer from "./component/footer/footer";
+import Header from "./component/header/header";
+import Section3 from "./component/main/section1 copy 2/section3";
+import Section4 from "./component/main/section1 copy 3/section4";
+import Section5 from "./component/main/section1 copy 4/section5";
+import Section2 from "./component/main/section1 copy/section2";
+import Section1 from "./component/main/section1/section1";
 
 function App() {
-
-
   return (
     <>
-      
-      <Header/>
-      <Footer/>
-      <Section1/>
-      <Section2/>
-      <Section3/>
-      <Section4/>
-      <Section5/>
-
+      <Header />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -1,16 +1,45 @@
+import "./footer.css";
 
-import './footer.css'
+import bilolrasm from "../../assets/logo (4).png";
 
 function Footer() {
-
-
   return (
     <>
-      
-      
+      <footer className="container">
+        <div className="xbet">
+          <section className="box">
+            <h1 className="g2">
+              <img src={bilolrasm} alt="" />
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing sed do eiusmod.
+            </p>
+          </section>
 
+          <section className="box">
+            <h2 className="g2">special catagories</h2>
+            <p className="f">water resistant</p>
+            <p className="f">charging system</p>
+            <p className="f">artificial leather</p>
+          </section>
+
+          <section className="">
+            <h2 className="g2">contact us</h2>
+            <p className="f">(808) 555-0111</p>
+            <p className="f">michelle.rivera@example.com</p>
+            <p className="f">http://www.codehow.com</p>
+          </section>
+
+          <section className="box">
+            <h2 className="g2">news latter</h2>
+            <input className="inp" placeholder="enter your email" type="text" />
+
+            <button className="vb">subscribe</button>
+          </section>
+        </div>
+      </footer>
     </>
-  )
+  );
 }
 
 export default Footer;
