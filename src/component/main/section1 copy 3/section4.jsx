94cbee536ed6c261f664ed150c8721d0b908df80
xwbaki <aslbekilhomov2009@gmail.com>
2026-01-19 14:56:28 +0500

    section 4 tamom

--- src/component/main/section1 copy 3/section4.jsx
+++ src/component/main/section1 copy 3/section4.jsx
@@ -1,4 +1,4 @@
-
+import op from '../../../assets/op.svg'
 import './section4.css'
 
 function Section4() {
@@ -6,8 +6,16 @@ function Section4() {
 
   return (
     <>
-      
-      
+      <div className='akmal'>
+        <img src={op} alt="" />
+        <div>
+          <h3 className='op'>Creative bag only for you.</h3>
+          <h1 className='do'>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+          <button className='faz'>see more</button>
+        </div>
+      </div>
+
 
     </>
   )
